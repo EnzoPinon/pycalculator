750ccea12e2dda4e380485a14122e671acbeae3f
EnzoPinon <mrlorenzopinon@gmail.com>
2023-05-10 20:02:03 +0800

    adding a welcome splash on startup

---
+++ calculate.py
@@ -0,0 +1,5 @@
+# Expected Output: Simple interface, calculator.
+
+
+#Make a simple UI upon opening
+print("==============================\nPy-Calc v1.0\n-by Lorenzo Pinon, in Python\n==============================")
